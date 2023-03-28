c6f60c37265436e3eba618a400f7d52a8a2fdffa
menescots <agatamenes@gmail.com>
2023-03-28 09:36:10 +0200

    add: gitignore file

---
+++ BookATable/BookATableApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookATableApp.swift
+//  BookATable
+//
+//  Created by Agata Menes on 28/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookATableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
