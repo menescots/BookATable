9090598f4b79903b9e73beca0186234fdeff1626
menescots <agatamenes@gmail.com>
2023-05-25 09:02:06 +0200

    READ.md update

--- BookATable/AuthenticationViews/SignInView.swift
+++ BookATable/AuthenticationViews/SignInView.swift
@@ -6,74 +6,91 @@
 //
 
 import SwiftUI
+import FirebaseAuth
 
-struct SignInView: View {
+struct SignInView: View, SecuredTextFieldParentProtocol {
+    var hideKeyboard: (() -> Void)?
+    
     @State private var email: String = ""
     @State private var password: String = ""
-       var body: some View {
-           VStack {
-               ZStack {
-                   Image("cutlery1")
-                       .resizable()
-                       .frame(width: 60, height: 60)
-                       .scaledToFit()
-               }
-               .frame(width: 100, height: 100)
-               .background(Color("DarkBlue"))
-               .cornerRadius(50)
-               .padding(.top, 100)
-               
-               Text("Log In")
-                   .foregroundColor(.black)
-                   .font(.custom("cochin", fixedSize: 45))
-                   .fontWeight(.medium)
-                   .padding(.bottom, 5)
-               
-               Text("Welcome back! It is time to eat something")
-                   .foregroundColor(.black)
-                   .font(.system(size: 15, weight: .thin))
-                   .padding(.bottom, 15)
-               TextField(
-                   "Enter email...",
-                   text: $email
-               )
-               .disableAutocorrection(true)
-               .autocapitalization(.none)
-               .clipShape(Capsule())
-               .padding(10)
-               .overlay(RoundedRectangle(cornerRadius:10.0).strokeBorder(Color("DarkBlue"), style: StrokeStyle(lineWidth: 1.0)))
-               .frame(width: UIScreen.main.bounds.width - 70, height: 44)
-               .padding(.bottom, 10)
-               
-               TextField(
-                   "Enter password...",
-                   text: $password
-               )
-               .disableAutocorrection(true)
-               .autocapitalization(.none)
-               .clipShape(Capsule())
-               .padding(10)
-               .overlay(RoundedRectangle(cornerRadius:10.0).strokeBorder(Color("DarkBlue"), style: StrokeStyle(lineWidth: 1.0)))
-               .frame(width: UIScreen.main.bounds.width - 70)
-               
-               NavigationLink {
-                   
-               } label: {
-                   HStack(alignment: .center) {
-                       Text("Sign in")
-                           .foregroundColor(.white)
-                           .font(.system(size: 24, weight: .light))
+    @EnvironmentObject var vm: UserStateViewModel
+    var body: some View {
+        ScrollView {
+            VStack {
+                ZStack {
+                    Image("cutlery1")
+                           .resizable()
+                           .frame(width: 60, height: 60)
+                           .scaledToFit()
                    }
-                   .frame(width: UIScreen.main.bounds.width - 130 , height: 44)
+                   .frame(width: 100, height: 100)
                    .background(Color("DarkBlue"))
-                   .cornerRadius(25)
+                   .cornerRadius(50)
+                   .padding(.top, 100)
+                   
+                   Text("Log In")
+                       .foregroundColor(.black)
+                       .font(.custom("cochin", fixedSize: 45))
+                       .fontWeight(.medium)
+                       .padding(.bottom, 5)
+                   
+                   Text("Welcome back! It is time to eat something")
+                       .foregroundColor(.black)
+                       .font(.system(size: 15, weight: .thin))
+                       .padding(.bottom, 15)
+                   TextField(
+                       "Enter email...",
+                       text: $email
+                   )
+                   .disableAutocorrection(true)
+                   .autocapitalization(.none)
+                   .clipShape(Capsule())
+                   .padding(10)
+                   .overlay(RoundedRectangle(cornerRadius:10.0).strokeBorder(Color("DarkBlue"), style: StrokeStyle(lineWidth: 1.0)))
+                   .frame(width: UIScreen.main.bounds.width - 70, height: 44)
+                   .padding(.bottom, 10)
+                   
+                   SecuredTextFieldView(text: $password, parent: self)
+                       .clipShape(Capsule())
+                       .padding(10)
+                       .overlay(RoundedRectangle(cornerRadius:10.0).strokeBorder(Color("DarkBlue"), style: StrokeStyle(lineWidth: 1.0)))
+                       .frame(width: UIScreen.main.bounds.width - 70)
+                       .padding(.bottom, 10)
+                   
+                   Button {
+                       guard password.count > 6, !email.isEmpty else {
+                           return
+                       }
+                       signIntoFirebase()
+                   } label: {
+                       HStack(alignment: .center) {
+                           Text("Sign in")
+                               .foregroundColor(.white)
+                               .font(.system(size: 24, weight: .light))
+                       }
+                       .frame(width: UIScreen.main.bounds.width - 130 , height: 44)
+                       .background(Color("DarkBlue"))
+                       .cornerRadius(25)
+                   }
+                   .padding(.top, 20)
+                   Spacer()
                }
-               .padding(.top, 20)
-               Spacer()
            }
-           
-           
        }
+    
+    func signIntoFirebase() {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            if error != nil {
+                print(error?.localizedDescription ?? "")
+            } else {
+                print("signed in ")
+                vm.isLogged = true
+                withAnimation() {
+                    vm.isMainLoginViewPresented = false
+                }
+            }
+        }
+    }
 }
 
 struct SignInView_Previews: PreviewProvider {
--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # BookATable
+# BookATable
+
+<br>
+<p align="center">
+<img src="./docs/mainLoginView.png" width="300" height="600" />
+<img src="./docs/register.png" width="300" height="600" /> 
+</p>
+<br>
+<p align="center">
+<img src="./docs/1.png" width="300" height="600" />
+<img src="./docs/2.png" width="300" height="600" /> 
+</p>
+<br>
+<p align="center">
+<img src="./docs/3.png" width="300" height="600" />
+<img src="./docs/4.png" width="300" height="600" /> 
+</p>
+<br>
+<p align="center">
+<img src="./docs/5.png" width="300" height="600" />
+<img src="./docs/6.png" width="300" height="600" /> 
+</p>
+<br>
+<p align="center">
+<img src="./docs/7.png" width="300" height="600" />
+<img src="./docs/8.png" width="300" height="600" /> 
+</p>
+<br>
+
